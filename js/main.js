363f17183ea1a442c2121cc652f7a3cf6ff79922
anggii <anggabos120@gmail.com>
2024-08-03 19:11:10 +0700

    pertama bro

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year in the span with id 'current-year'
+// document.getElementById('current-year').textContent = currentYear;
